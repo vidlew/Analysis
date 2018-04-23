10041be85f792e9b9252cd37cc0686efc3752377
David <vidlew@outlook.com>
2018-04-22 22:35:01 -0300

    new file:   Symbolic.hs

---
+++ Symbolic.hs
@@ -0,0 +1,80 @@
+{-# LANGUAGE GADTs, StandaloneDeriving #-}
+
+module Symbolic where
+
+infixl 5 :+
+infixr 5 :-
+infixl 7 :*
+infixr 7 :/
+
+data Expr a where
+  Var :: String -> Expr a
+  Const :: a -> Expr a
+  (:+) :: (Num a) => Expr a -> Expr a -> Expr a
+  (:*) :: (Num a) => Expr a -> Expr a -> Expr a
+  (:-) :: (Num a) => Expr a -> Expr a -> Expr a
+  (:/) :: (Fractional a) => Expr a -> Expr a -> Expr a 
+  Pi :: (Floating a) => Expr a
+  Exp :: (Floating a) => Expr a -> Expr a
+  Log :: (Floating a) => Expr a -> Expr a
+  Sin :: (Floating a) => Expr a -> Expr a
+  Cos :: (Floating a) => Expr a -> Expr a
+  Asin :: (Floating a) => Expr a -> Expr a
+  Acos :: (Floating a) => Expr a -> Expr a
+  Atan :: (Floating a) => Expr a -> Expr a
+  Sinh :: (Floating a) => Expr a -> Expr a
+  Cosh :: (Floating a) => Expr a -> Expr a
+  Asinh :: (Floating a) => Expr a -> Expr a
+  Acosh :: (Floating a) => Expr a -> Expr a
+  Atanh :: (Floating a) => Expr a -> Expr a
+
+deriving instance (Show a) => Show (Expr a)
+
+instance (Num a) => Num (Expr a) where{
+  fromInteger = Const . fromInteger
+; (+) = (:+)
+; (*) = (:*)
+; (-) = (:-)
+}
+
+instance (Fractional a) => Fractional (Expr a) where{
+  fromRational = Const . fromRational
+; (/) = (:/)
+}
+
+instance (Floating a) => Floating (Expr a) where{
+  pi = Pi
+; exp = Exp
+; log = Log
+; sin = Sin
+; cos = Cos
+; asin = Asin
+; acos = Acos
+; atan = Atan
+; sinh = Sinh
+; cosh = Cosh
+; asinh = Asinh
+; acosh = Acosh
+; atanh = Atanh
+}
+
+partial :: (Num a) => String -> Expr a -> Expr a
+partial _ (Const x) = 0
+partial s (Var t) = if s==t then 1 else 0
+partial s (x:+y) = (partial s x) + (partial s y)
+partial s (x:*y) = (partial s x)*y + x+(partial s y)
+partial s (x:/y) = ((partial s x)*y - x*(partial s y)) / (y*y)
+partial _ Pi = 0
+partial s (Exp x) = (partial s x) * (Exp x)
+partial s (Log x) = (partial s x) / x
+partial s (Sin x) = (partial s x) * (Cos x)
+partial s (Cos x) = -(partial s x) * (Sin x)
+partial s (Asin x) = (partial s x) / (sqrt $ 1-x*x)
+partial s (Acos x) = -(partial s x) / (sqrt $ 1-x*x)
+partial s (Atan x) = (partial s x) / (1+x*x)
+partial s (Sinh x) = (partial s x) * (Cosh x)
+partial s (Cosh x) = (partial s x) * (Sinh x)
+partial s (Asinh x) = (partial s x) * (sqrt $ x*x+1)
+partial s (Acosh x) = (partial s x) * (sqrt $ x*x-1)
+partial s (Atanh x) = -(partial s x) / (1-x*x)
+
